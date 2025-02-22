a7ce459dd1da8c7cdd5ed7fdad8694d02ecdf38c
yazkilito@gmail.com <yazkilito@gmail.com>
2025-02-22 16:48:08 +0100

    git add main

--- main.py
+++ main.py
@@ -16,11 +16,33 @@ CLIENT_SECRET = "cnhhv1qwdxfjc8smmtjnbieg5c9p57"
 
 
 
+t1  =  TwitchApi(CLIENT_ID,CLIENT_SECRET)                 
+TwitchApi.getHeaders(t1)
+
+idt = TwitchApi.getUserId(t1, "talmo")
+data = TwitchApi.getClips(
+    t1,
+    idt,
+    
+    filters={"started_at": "2024-01-30T00:00:00Z", "ended_at": "2025-02-09T00:00:00Z", "first": 3},
+    min_duration=20,
+    max_duration=60
+)
+
+# Affichage propre des données sous forme JSON
+TwitchApi.downloadClipWithAudio(t1,data)
+
+for files in os.listdir("C:\\Users\\User\\Desktop\\Cours\\Framework Web\\GROOT\\Clips"):
+    v1 = VideoProcessor(f"Clips/{files}")
+    v1.process_video(1080,1920)
+
+
+
 ti1 = TiktokApi()
 
 ti1.startDriver()
 ti1.login("yazkilito@gmail.com","Didoleboss12$")
-ti1.uploadVideo("C:\\Users\\User\\Desktop\\Cours\\Framework Web\\GROOT\\Processed_clips\\97dc3e85-7503-4d78-ba66-e5e87d3205bc_processed.mp4","test1")
+ti1.uploadVideo("Processed_clips\97dc3e85-7503-4d78-ba66-e5e87d3205bc_processed.mp4","test1")
 
 
 
