8dddc12e908dab82f30ad42ef7041a47197ba4c8
yazkilito@gmail.com <yazkilito@gmail.com>
2025-02-22 13:50:36 +0100

    update video processor with the use of webcam processor instead

--- Edit/Video_processor.py
+++ Edit/Video_processor.py
@@ -2,10 +2,14 @@
 # src/edit/video_processor.py
 
 from moviepy.editor import VideoFileClip, CompositeVideoClip, ColorClip
+from Edit.Web_cam_processor import WebCamProcessor
 from PIL import Image, ImageFilter
 import logging
+import uuid
 import numpy as np
 import cv2
+import os 
+
 
 
 # Setting up logging for tracking the processing steps
@@ -13,17 +17,19 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 class VideoProcessor:
 
-    def __init__(self,format_type,clip):
-        self.format_type = format_type
-        self.clip = cv2.VideoCapture(clip)
-        self.clip_width = int(self.clip.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.clip_height = int(self.clip.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.web_cam_coordinate = []
-        self.web_cam_frame = self.get_web_cam_frame_by_coordinate
+    def __init__(self,clipUrl):
+        
+        self.clip_url =  clipUrl
+        self.clipId = str(uuid.uuid4())
+        self.clipVideo = VideoFileClip(self.clip_url)
+        self.clip_width , self.clip_height = self.clipVideo.size
+        self.web_cam_video = None 
+        self.content_video = None
+
   
     def extract_frames(self):
         frames_list = []
-        clip = self.clip
+        clip = self.clipVideo
 
         if not clip.isOpened():
             print("Erreur : Impossible d'ouvrir la vidéo.")
@@ -40,119 +46,95 @@ class VideoProcessor:
         return frames_list
 
     def get_first_frame(self):
-        if not self.clip.isOpened():
+        if not self.clipVideo.isOpened():
             logging.error("Erreur: Impossible d'ouvrir la vidéo.")
             return None
-        ret, first_frame = self.clip.read()
+        ret, first_frame = self.clipVideo.read()
         if not ret:
             logging.error("Erreur: Impossible de lire la première frame")
             return None  # Retourne None si l'image n'a pas pu être lue
 
         return first_frame
     def get_clip(self):
-        return self.clip
+        return self.clipVideo
 
     def get_clip_coordinate(self):
         return (self.clip_width, self.clip_height)
     
-    def get_clicked_coordinate(self,event,x,y,flags,param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-           self.web_cam_coordinate.append((x,y))
-    
 
+    def extract_web_cam(self):
+        web_cam_processor = WebCamProcessor(self.clip_url)
+        web_cam_processor.detect_faces()
 
-    def get_point_manually(self):
-        ret, self.frame = self.clip.read()
-        if not ret:
-            print("Erreur : Impossible de capturer une image.")
-            return []
-        cv2.namedWindow("Frame",cv2.WINDOW_NORMAL)
-        cv2.imshow("Frame", self.frame)
-        cv2.setMouseCallback("Frame", self.get_clicked_coordinate)
-
-        while True:
-            key = cv2.waitKey(1) & 0xFF
-            if key == 27 or len(self.web_cam_coordinate) == 4:  # Quitter avec ESC
-                break
-
-        cv2.destroyAllWindows()
-        return self.web_cam_coordinate
-    
-    
-    """
-    def sub_frame(self):
-        sub_frame_list = []
-        frame = self.get_first_frame()  # Récupère la frame complète
-
-        height, width = self.clip_height, self.clip_width  # Dimensions de la frame
-
-        third_width = width // 3  # Divise la largeur en 3 parties égales
-        third_height = height // 3  # Divise la hauteur en 3 parties égales
-
-        # Prendre uniquement la partie gauche et droite
-        left_part = frame[:, 0:third_width]  # Partie gauche
-        right_part = frame[:, 2 * third_width:width]  # Partie droite
-
-        # Découper chaque partie en 3 sous-parties en hauteur
-        for i in range(3):
-            sub_frame_left = left_part[i * third_height:(i + 1) * third_height, :]
-            sub_frame_right = right_part[i * third_height:(i + 1) * third_height, :]
-
-            sub_frame_list.append(sub_frame_left)  # Ajouter la sous-image gauche
-            sub_frame_list.append(sub_frame_right)  # Ajouter la sous-image droite
-
-        return sub_frame_list  # Retourne la liste des 6 sous-images
-
-    """
-
-    def detect_faces(frame):
-        """ Détecte les visages avec un modèle DNN plus précis. """
-        
-        # Chemins corrects des fichiers du modèle
-        prototxt_path = "Edit\\sample\\deploy.prototxt"
-        model_path = "Edit\\sample\\res10_300x300_ssd_iter_140000.caffemodel"
-
-        # Charger le modèle
-        net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
-
-        # Récupérer la taille de l'image
-        h, w = frame.shape[:2]
-
-        # Préparer l'image pour le réseau de neurones
-        blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0, size=(300, 300), mean=(104.0, 177.0, 123.0))
-
-        # Envoyer l'image dans le réseau de neurones
-        net.setInput(blob)
-        detections = net.forward()
-
-        # Traiter les résultats
-        for i in range(detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
-            if confidence > 0.5:
-                box = detections[0, 0, i, 3:7] * [w, h, w, h]
-                x, y, x2, y2 = box.astype("int")
-                cv2.rectangle(frame, (x, y), (x2, y2), (0, 255, 0), 2)
-
-        return frame
-
-
-    def get_web_cam_frame_by_coordinate(self):
-        if len(self.web_cam_coordinate) != 4:
-            raise ValueError("Erreur : Les coordonnées sont pas renseigner ou manquant.")
-
-        points = np.array(self.web_cam_coordinate, dtype=np.int32)
-    
-        x_min = np.min(points[:, 0])
        
-        y_min = np.min(points[:, 1])
-        x_max = np.max(points[:, 0])
-        y_max = np.max(points[:, 1])
 
-        # Découper la sous-frame
-        sub_frame = self.get_first_frame()[y_min:y_max, x_min:x_max]
+        web_cam_coordinate = web_cam_processor.web_cam_coordinate
 
-        return sub_frame
+        web_cam_clip = self.clipVideo.crop(x1=web_cam_coordinate[2],y1=web_cam_coordinate[0],x2=web_cam_coordinate[3],y2=web_cam_coordinate[1])
+
+        web_cam_clip_path = f'in_process_clips/{self.clipId}_cam.mp4'
+        web_cam_clip.write_videofile(web_cam_clip_path,codec="libx264",fps=30)
+
+        self.web_cam_video = VideoFileClip(web_cam_clip_path)
+
     
+
+    def crop_to_short_format(self, target_width, target_height):
+        original_ratio = self.clipVideo.w / self.clipVideo.h
+        target_ratio = 9 / 16  # Format TikTok
+
+        if original_ratio > target_ratio:
+            # Trop large, on coupe sur les côtés
+            new_width = int(self.clipVideo.h * target_ratio * 1.4)
+            x1 = (self.clipVideo.w - new_width) // 2
+            x2 = x1 + new_width
+            y1, y2 = 0, self.clipVideo.h
+        else:
+            # Trop haut, on coupe en haut et en bas
+            new_height = int(self.clipVideo.w / target_ratio)
+            y1 = (self.clipVideo.h - new_height) // 2
+            y2 = y1 + new_height
+            x1, x2 = 0, self.clipVideo.w
+
+        # Rogner la vidéo
+        cropped_video = self.clipVideo.crop(x1=x1, y1=y1, x2=x2, y2=y2)
+
+        content_clip_path = f"in_process_clips/{self.clipId}_content.mp4"
+        # Redimensionner exactement à 1080x1920
+        content_clip = cropped_video.resize((target_width, int((2 * target_height) / 3)))
+        content_clip.write_videofile(content_clip_path, codec="libx264", fps=30)
+
+        self.content_video = VideoFileClip(content_clip_path)
+
+    def process_video(self):
+        """ Ajuster la webcam : même largeur que la vidéo de contenu, et 1/3 de sa hauteur """
+
+
+
+        processed_video_path = f'Processed_clips/{self.clipId}_processed.mp4'
+        empty_clip = ColorClip(size=(1080,1920), color=(0,0,0),duration=self.content_video.duration)
+
+
+        web_cam = self.web_cam_video.resize(width=1080)
+        if web_cam.h > 640:
+            web_cam = web_cam.crop(y1=0, y2=640)
+        else:
+            web_cam = web_cam
+        web_cam = web_cam.set_position(("center", 0))
+
+
+        content = self.content_video.set_position(("center",1920-1280))
+        final_clip = CompositeVideoClip([empty_clip,content, web_cam])
+        final_clip.write_videofile(processed_video_path, codec="libx264", fps=30)
+
+        os.remove(f'in_process_clips/{self.clipId}_cam.mp4')
+        os.remove(f'in_process_clips/{self.clipId}_content.mp4')
+    
+
+    
+
+
+   
 
 
 
