89975d58455741c48b324ba134bc8dd025d8e538
hugohebbinckuys <hugo29.heb@gmail.com>
2025-02-27 00:27:32 +0100

    cleaning inscription.py file

--- bdd/inscription.py
+++ bdd/inscription.py
@@ -12,14 +12,6 @@ CORS(app) # pour autoriser les requetes post d'autres origines que le servuer
 def home(): 
     return "Bienvenu brother"
 
-# def get_db(): 
-#     db = getattr(g, '_database', None) #retourne le nom d'un attribut de l'objet
-#     if db is None : 
-#         print ("bdd non connectée, connexion...")
-#         db = g._database = mysql.connector.connect(host="localhost", user="root", password="", database=DATABASE)
-#         print ("bdd connectée")
-#         return db
-
 def get_db(): 
     db = getattr(g, '_database', None)
     if db is None:
@@ -85,54 +77,6 @@ def insert_user () :
         logging.error(f'Erreur de requete : {e}')
         db.rollback() # pour éviter de push la requete vu qu'elle va flop 
         return jsonify({"error" : "query error"}), 500 # retourne erreur 500  
-
-    
-
-
-
-# @app.route('/insert_new_user', methods=["POST"]) 
-# # def insert_new_user (username, password_not_hashed, tiktok_username="", tiktok_password_not_hashed="") : 
-# def insert_new_user () : # test sans les arguments en les donnant et y accedant vaec request pour voir si ca marche  
-
-#     recup_data = request.get_json() # recup des données envoyés depuis vue 
-
-#     username = recup_data.get('username') 
-#     password_not_hashed = recup_data.get('password')
-#     tiktok_username = recup_data.get('tiktok_username', '')  # deuxieme arg pour dire que c vide p/defaut 
-#     tiktok_password_not_hashed = recup_data.get('tiktok_password', '')
-
-#     print (f"username : {username}") # ici username = None 
-
-#     db = get_db() 
-#     cursor = db.cursor() 
-#     try :
-#         if (user_already_exists(username)) :
-#             logging.warning(f"user {username} already exists.")
-#             return jsonify({"error": "Username already exists"}), 400
-
-
-#         query = ("Insert into user (username, password, tiktok_username, tiktok_password) VALUES (%s, %s, %s, %s)") 
-#         values = (username, password_not_hashed, tiktok_username, tiktok_password_not_hashed)
-
-#         insert = cursor.execute(query, values) 
-#         db.commit() 
-#         cursor.close() 
-
-#         logging.info(f"User '{username}' inserted successfully.")
-#         return jsonify({"message": "User inserted "}), 400
-
-#     except mysql.connector.Error as e : 
-#         logging.error(f" Erreur requete  {e}")
-#         db.rollback()  # Annule la requete si erreur jcrois 
-
-
-
-
-
-
-# with app.app_context() : # pour executer get_db() dans un contexte valide 
-#     db = get_db()
-#     insert_new_user("yahugozid", "yazid_password", "yazid_tiktok", "yazid_password_tiktok") 
 
 if __name__ == '__main__' : 
     app.run(debug=True)
