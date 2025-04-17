d2a22414f78ec3e0c8a04ae48cf7db4daf3e0600
yazkilito@gmail.com <yazkilito@gmail.com>
2025-04-17 15:07:39 +0100

    add setup to allow import of every part of the app as package to solve import problems

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GROOT",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+        "mysql-connector-python",
+        "flask_cors",
+        "flask_bcrypt",
+        "requests",
+        "flask",
+        "requests",
+        "moviepy",
+        "selenium",
+        "webdriver-manager",
+        "opencv-python",
+        "pillow",
+        "streamlink",
+        "werkzeug",
+        "numpy",
+        "mysql-connector-python",
+        
+    ],
+)
