4bcf457cee2b9c91bfadda31949e74b9c37710c9
yazkilito@gmail.com <yazkilito@gmail.com>
2025-06-09 20:52:04 +0200

    transfered create_app into run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,24 @@
-from backend import create_app
+
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+from Routes.auth_routes import AuthRoutes
+import os
+import sys
+
+
+load_dotenv(dotenv_path="backend/.env")
+def create_app():
+    app = Flask(__name__) #création application flask 
+
+
+    CORS(app)
+
+    app.register_blueprint(AuthRoutes.auth_bp)
+
+
+    return app
 
 app = create_app()
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
