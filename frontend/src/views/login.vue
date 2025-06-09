1e4a1f67acccbff80abb812ae5ba3b961940d336
yazkilito@gmail.com <yazkilito@gmail.com>
2025-06-09 20:56:50 +0200

    update login variable field to match new userservices

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -4,14 +4,14 @@ import { ref } from 'vue'
 import router from '@/router'
 import { useTwitokStore } from '@/store/twitokStore'; //import. store 
 
-const username = ref("")
-const password = ref("")
+const user_email = ref("")
+const user_password = ref("")
 const twitokStore = useTwitokStore() // vient du store 
 
 
 const connect = async () => {
     try {
-        const dataToSend = {username: username.value, password: password.value}
+        const dataToSend = {user_email: user_email.value, user_password: user_password.value}
         const response = await axios.post('http://127.0.0.1:5000/login', dataToSend)
         console.log("tentative de connexion de l'user : ", response.data)
         twitokStore.autorized()
@@ -29,8 +29,8 @@ const connect = async () => {
 
 <form class="form-container" action="">
         <label> Sign up </label>
-        <input type="text" v-model="username" placeholder="username" required class="input-field">
-        <input type="password" v-model="password" placeholder="password" required class="input-field">
+        <input type="text" v-model="user_email" placeholder="email" required class="input-field">
+        <input type="password" v-model="user_password" placeholder="password" required class="input-field">
         <input type="submit" @click.prevent="connect()" class="submit-btn"> <!-- prevent c pour éviter que la page se recharge a chauqe fois qu'on soumet le form -->
     </form>
 
