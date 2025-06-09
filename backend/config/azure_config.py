05e207f04fa1eb7ec5ccb29e5bb59742e32f62d7
yazkilito@gmail.com <yazkilito@gmail.com>
2025-06-09 20:54:56 +0200

    remove dotenv

--- backend/config/azure_config.py
+++ backend/config/azure_config.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv(dotenv_path="backend/.env")
+
 # Configuration pour Azure SQL Database
 class AzureConfig:
     
