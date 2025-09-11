850c1506cb888c1d5000f1ca831d7c77d3296061
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-11 20:21:04 +0200

    add send_clips_url functionnality in controller

--- backend/Controller/clip_controller.py
+++ backend/Controller/clip_controller.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from Service.clip_services import ClipServices
 from flask import request, jsonify
@@ -10,7 +11,6 @@ class ClipController():
 
     @staticmethod
     def fetch_clip(data=None):
-        print("s")
         if data is None:
             data_received = request.json
             streamer_name = data_received.get('streamer_name')
@@ -49,3 +49,20 @@ class ClipController():
                 "message" :"clip added !",
 
             }), 201
+    
+
+    @staticmethod
+    def send_clips_urls():
+        if request.method == 'OPTIONS' : #requete options au back end avant POST quand on fait une requete post 
+            return '', 200
+        try :
+            liste_urls = [] 
+            path = os.path.join("backend","data","fetch_clips")
+            for file in os.listdir(path) :   
+                liste_urls.append(file)
+            return jsonify ({"clipsUrls": liste_urls})
+        except Exception as e : 
+            logging.error(f"Erreur lors de l'envoie de l'url des clips : {e}")
+            return ({"error": "Erreur lors de l'envoi de l'url des clips"}), 500
+
+        
