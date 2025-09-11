9a289f32f46884a04e70cb954b9b179ec9e64503
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-11 20:21:13 +0200

    add send_clips_ulrs api route

--- backend/routes/clip_routes.py
+++ backend/routes/clip_routes.py
@@ -7,3 +7,4 @@ class ClipRoutes:
 
     clip_bp = Blueprint("clip",__name__)
     clip_bp.route("/recup_infos_clips",methods=["POST"])(ClipController.fetch_clip)
+    clip_bp.route("/send_clips_urls",methods=["GET"])(ClipController.send_clips_urls)
