2abab7b1be530f1c4a05caade2ddf3e31767560e
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-13 19:59:02 +0200

    remove fetch_clip_route from clip blueprint

--- backend/routes/clip_routes.py
+++ backend/routes/clip_routes.py
@@ -6,5 +6,4 @@ from Controller.clip_controller import ClipController
 class ClipRoutes:
 
     clip_bp = Blueprint("clip",__name__)
-    clip_bp.route("/recup_infos_clips",methods=["POST"])(ClipController.fetch_clip)
     clip_bp.route("/send_clips_urls",methods=["GET"])(ClipController.send_clips_urls)
