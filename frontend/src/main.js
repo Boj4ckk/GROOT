403979dd873c129f32f18ea02114a74f807581e7
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-18 13:18:55 +0200

    define baseurl (backend) for all routes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,13 @@ import router from './router'
 import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import axios from "axios"
+
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:5000'
 
 const app = createApp(App)
-
 const pinia = createPinia() 
 app.use(pinia)
 app.use(router)
