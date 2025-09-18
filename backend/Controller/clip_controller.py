0d9c2bf63bc892e9ec5defd8d8595f59378feeee
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-18 13:16:17 +0200

    add jwt token requiered + add blob storage architecture instead of folder for fetched clips

--- backend/Controller/clip_controller.py
+++ backend/Controller/clip_controller.py
@@ -1,24 +1,26 @@
 import logging
 import os
+from flask import request
 from Service.clip_services import ClipServices
+from Service.blob_service import BlobStorageService
 from flask import request, jsonify
 from config.azure_config import SessionLocal
+from middlewares.auth_middleware import jwt_required
 
 
 class ClipController():
 
     @staticmethod
+    @jwt_required
     def send_clips_urls():
         if request.method == 'OPTIONS' : #requete options au back end avant POST quand on fait une requete post 
             return '', 200
         try :
-            liste_urls = [] 
-            path = os.path.join("backend","data","fetch_clips")
-            for file in os.listdir(path) :   
-                liste_urls.append(file)
-            return jsonify ({"clipsUrls": liste_urls})
+            blob_service = BlobStorageService()
+            user_fetched_clip_data = blob_service.get_user_fetched_clips(request.user_id)
+        
         except Exception as e : 
             logging.error(f"Erreur lors de l'envoie de l'url des clips : {e}")
             return ({"error": "Erreur lors de l'envoi de l'url des clips"}), 500
-
+        return user_fetched_clip_data
         
