62e50d5f0fe0e6ad1db5b611280755aefcf6fea5
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-18 13:15:28 +0200

    update auth controler to create jwt token when logged in

--- backend/Controller/auth_controller.py
+++ backend/Controller/auth_controller.py
@@ -1,8 +1,9 @@
 
 from Controller.user_controller import UserController
-from flask import request, jsonify
+from flask import make_response, request, jsonify
 from Service.user_services import UserService
 from config.azure_config import SessionLocal
+from Adapter.jwt_adapter import JwtAdapter
 
 class AuthController:
 
@@ -21,10 +22,22 @@ class AuthController:
         with SessionLocal() as db:
             user_service = UserService(db)
             user = user_service.authentificate(data["user_email"], data["user_password"])
-        if not user:
-            return jsonify({"Error" :" User not found !"}), 401
-        
-        return jsonify({
-            "message": "User connected"
-        }),200
+            if not user:
+                return jsonify({"error": "Utilisateur non trouvé"}), 404
+
+            payload = {"user_id": user.id_user}
+            token_jwt  = JwtAdapter.encode(payload)
+
+            response = make_response(jsonify({"message":"Logged in"}))
+            response.set_cookie(
+                "access_token", 
+                token_jwt,
+                httponly=True,
+                secure=False,  # Mettre à True en production avec HTTPS
+                samesite='Lax',  # Important pour CORS
+                max_age=3600,
+                path='/'  # Assure que le cookie est disponible sur toutes les routes
+            )
+            return response
+    
         
