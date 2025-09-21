c7bce8a362faa1a67a66cad9c8b19dc61caff038
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-21 20:44:40 +0200

    update app vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,10 +6,12 @@ import navbar from './components/navbar.vue';
 </script>
 
 <template>
+  <div class="px-2 md:px-54 pt-40 md:pt-72">
+    <navbar /> 
+    <router-view/> 
 
-  <navbar/> 
-  <router-view/> 
-
-  <br><br><br><br><br>
+    <br><br><br><br><br>
+  </div>
+ 
 
 </template>
