dbdaed98578c5e68580a3d355b2a43004aaa0796
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-21 20:44:50 +0200

    import main.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+
+import '@/assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
 import axios from "axios"
 
 
