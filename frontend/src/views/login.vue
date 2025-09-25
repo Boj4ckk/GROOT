82e6089edd4c42d493c863875e0f3aa48c876520
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-25 18:31:23 +0200

    re desing login and register pages

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { ref } from 'vue'
 import router from '@/router'
 import { useTwitokStore } from '@/store/twitokStore'; //import. store 
+import {AtSymbolIcon} from '@heroicons/vue/24/solid'
 
 const user_email = ref("")
 const user_password = ref("")
@@ -26,15 +27,40 @@ const connect = async () => {
 </script>
 
 <template>
+<div class="">
+        <div class='flex justify-center font-inter font-bold pt-20 md:pt-1 text-[20px] md:text-[35px] animate-fade-in-up'>
+            Sign In
+        </div>
+        <div class=' flex justify-center py-4 animate-fade-in-up'>
+            <div class=' border-1 border-black flex  py-1 px-1'>
+                    <at-symbol-icon class=" h-4 w-5 md:h-8 md:w-9 text-gray-900" />
+                    <h7 class='px-2 md:px-3 md:pt-1 font-inter font-medium text-[12px] md:font-semibold md:text-[17px]'>
+                        Sign in with google
+                    </h7>
+                    
+                
+            </div>
+        </div>
 
-<form class="form-container" action="">
-        <label> Sign up </label>
-        <input type="text" v-model="user_email" placeholder="email" required class="input-field">
-        <input type="password" v-model="user_password" placeholder="password" required class="input-field">
-        <input type="submit" @click.prevent="connect()" class="submit-btn"> <!-- prevent c pour éviter que la page se recharge a chauqe fois qu'on soumet le form -->
-    </form>
+        <div class=' flex flex-col justify-center items-center '>
+             <form class="flex flex-col justify-between  md:px-64 animate-fade-in-up" action="">
+                 <div class=" flex flex-col px-4 py-1 items-center ">
+                    <div class=" w-64 md:w-96 font-inter font-light text-[15px] md:text-[20px]">Email</div>
+                    <input class='rounded-lg py-1 w-64 md:w-96 input-field border-1 border-black px-2' type="text" v-model="username"  required  >
+                </div>
+                <div class="flex flex-col px-4 py-1 items-center pb-5  ">
+                    <div class=" w-64 md:w-96 font-inter font-light text-[15px] md:text-[20px]">Password</div>
+                    <input class='rounded-lg py-1 w-64 md:w-96 input-field border-1 border-black px-2'  type="password" v-model="password"  required >
+                </div>
+              
+               
+                
+                
+            </form>
+            <input type="submit" @click.prevent="insert_user()" class="border-1 border-black w-48 md:w-64 py-1 md:text-[20px] animate-fade-in-up" value='Register'> <!-- prevent c pour éviter que la page se recharge a chauqe fois qu'on soumet le form -->
 
-    <router-link 
-    to="register">Not yet an account ?</router-link>
-
+         <router-link class="no-underline pt-2 text-black font-inter font-extralight text-[12px] md:text-[18px] animate-fade-in-up" to="register">Not registered ? Sign up</router-link>
+        </div>
+       
+    </div>
 </template>
--- frontend/src/views/register.vue
+++ frontend/src/views/register.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 import apiClient from '@/api';
 import router from '@/router';
 import { useTwitokStore } from '@/store/twitokStore'; //import. store 
+import {AtSymbolIcon} from '@heroicons/vue/24/solid'
 
 const twitokStore = useTwitokStore() // vient du store 
 // const authorizedConnection = twitokStore.authorizedConnection
@@ -33,17 +34,46 @@ const insert_user = async () => {
 
 <template>
 
-    <form class="form-container" action="">
-        <label> Sign up </label>
-        <input type="text" v-model="username" placeholder="username" required class="input-field">
-        <input type="password" v-model="password" placeholder="password" required class="input-field">
-        <input type="text" v-model="tiktok_username" placeholder="tiktok username" class="input-field">
-        <input type="password" v-model="tiktok_password" placeholder="tiktok password" class="input-field">
-        <input type="submit" @click.prevent="insert_user()" class="submit-btn"> <!-- prevent c pour éviter que la page se recharge a chauqe fois qu'on soumet le form -->
-    </form>
+    <div class="">
+        <div class='flex justify-center font-inter font-bold pt-20 md:pt-1 text-[20px] md:text-[35px] animate-fade-in-up'>
+            Create an account
+        </div>
+        <div class=' flex justify-center py-4 animate-fade-in-up'>
+            <div class=' border-1 border-black flex  py-1 px-1'>
+                    <at-symbol-icon class=" h-4 w-5 md:h-8 md:w-9 text-gray-900" />
+                    <h7 class='px-2 md:px-3 md:pt-1 font-inter font-medium text-[12px] md:font-semibold md:text-[17px]'>
+                        Sign up with google
+                    </h7>
+                    
+                
+            </div>
+        </div>
 
-    <router-link to="connection">Already an account ?</router-link>
+        <div class=' flex flex-col justify-center items-center '>
+             <form class="flex flex-col justify-between  md:px-64 animate-fade-in-up" action="">
+                 <div class=" flex flex-col px-4 py-1 items-center ">
+                    <div class=" w-64 md:w-96 font-inter font-light text-[15px] md:text-[20px]">Email</div>
+                    <input class='rounded-lg py-1 w-64 md:w-96 input-field border-1 border-black px-2' type="text" v-model="username"  required  >
+                </div>
+                <div class=" flex flex-col px-4 py-1 items-center ">
+                    <div class=" w-64 md:w-96 font-inter font-light text-[15px] md:text-[20px]">Password</div>
+                    <input class='rounded-lg py-1 w-64 md:w-96 input-field border-1 border-black px-2'  type="password" v-model="password"  required >
+                </div>
+                <div class=" flex flex-col px-4 py-1 pb-5 items-center ">
+                    <div class=" w-64 md:w-96 font-inter font-light text-[15px] md:text-[20px]">Re-enter Password</div>
+                    <input class='rounded-lg py-1  w-64 md:w-96 input-field border-1 border-black px-2'  type="password" v-model="password"  required >
+                </div>
+              
+               
+                
+                
+            </form>
+            <input type="submit" @click.prevent="insert_user()" class="submit-btn border-1 border-black w-48 md:w-64 py-1 md:text-[20px] animate-fade-in-up" value='Register'> <!-- prevent c pour éviter que la page se recharge a chauqe fois qu'on soumet le form -->
 
+         <router-link class="no-underline pt-2 text-black font-inter font-extralight text-[12px] md:text-[18px] animate-fade-in-up" to="login">Already an account ?</router-link>
+        </div>
+       
+    </div>
 
 </template>
 
