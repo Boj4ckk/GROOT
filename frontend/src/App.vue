9771e6650172fd0fa5c22c94bce740060902b6ad
yazkilito@gmail.com <yazkilito@gmail.com>
2025-09-25 18:30:33 +0200

    update app padding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ import navbar from './components/navbar.vue';
 </script>
 
 <template>
-  <div class="px-[15px] md:!px-64 pt-40 md:pt-72 ">
+  <div class=" px-[15px] pt-10 md:!px-64 md:pt-40 ">
     <navbar class=" px-[15px] md:px-64" /> 
     <router-view class=""/> 
 
