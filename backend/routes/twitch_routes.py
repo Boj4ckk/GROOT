290ea3916d1ca85ab692af4d2929e721ef52a5b2
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-07 22:46:41 +0200

    add search games and searcj streamers

--- backend/routes/twitch_routes.py
+++ backend/routes/twitch_routes.py
@@ -6,5 +6,7 @@ class TwitchRoutes:
 
     twitch_bp = Blueprint("twitch",__name__)
     twitch_bp.route("/recup_infos_clips",methods=["POST"])(TwitchController.fetch_clip)
+    twitch_bp.route("/search_games",methods=["GET"])(TwitchController.search_games)
+    twitch_bp.route("/search_streamers",methods=["GET"])(TwitchController.search_streamers)
 
 
