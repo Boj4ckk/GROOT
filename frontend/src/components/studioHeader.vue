5a1d5db559a311b50a79807ae8178c66d7f1ad17
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-07 22:46:59 +0200

    studioheader update

--- frontend/src/components/studioHeader.vue
+++ frontend/src/components/studioHeader.vue
@@ -18,23 +18,24 @@ const isCurrentPath = (title) =>{
 
 
 <template>
-     <header class=" bg-black shadow-lg fixed top-14 left-0 right-0 z-50 h-[35px] flex justify-center items-center">
-        <nav id="mainNav" class="  lg:flex  max-w-lg justify-center  mr-8">
-                    <div class="space-x-12 md:space-x-16 flex justify-between">
+     <header class=" bg-black shadow-lg fixed top-14 left-0 right-0 z-50 h-[35px]  flex items-center justify-center">
+         <!-- Navigation-->
+        <nav id="mainNav" class="  lg:flex  lg:max-w-lg flex justify-center items-center  md:mr-8 w-full bg-blue-700">
+                    <div class="space-x-12 md:space-x-16 flex justify-center items-center w-full bg-red-700 md:justify-between">
                          <div :class="[
                             isCurrentPath('filtrate') 
-                                ? 'text-white font-inter font-medium text-[13px] md:text-[16px]' 
-                                : 'text-gray-400 font-inter font-medium text-[13px] md:text-[16px]'
+                                ? 'text-white font-inter font-medium text-[13px] md:text-[16px] text-center w-full flex justify-center items-center' 
+                                : 'hidden md:block text-gray-400 font-inter font-medium text-[13px] md:text-[16px]'
                             ]">Filtrate</div>
                          <div :class="[
                             isCurrentPath('clip_selection') 
                                 ? 'text-white font-inter font-medium text-[13px] md:text-[16px]' 
-                                : 'text-gray-400 font-inter font-medium text-[13px] md:text-[16px]'
+                                : 'hidden md:block text-gray-400 font-inter font-medium text-[13px] md:text-[16px]'
                             ]">Selection</div>
                          <div :class="[
                             isCurrentPath('video_edit') 
                                 ? 'text-white font-inter font-medium text-[13px] md:text-[16px]' 
-                                : 'text-gray-400 font-inter font-medium text-[13px] md:text-[16px]'
+                                : 'hidden md:block text-gray-400 font-inter font-medium text-[13px] md:text-[16px]'
                             ]">Editing</div>
                     </div>
                 </nav>
