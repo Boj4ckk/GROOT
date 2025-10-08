68184a258a1b2eacc17cf2647c4f3f0d3962808a
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-08 23:23:00 +0200

    update app vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import navbar from './components/navbar.vue';
-
-
 </script>
 
 <template>
-  <div class=" px-[15px] pt-10 md:!px-64 md:pt-40 ">
-    <navbar class=" px-[15px] md:px-64" /> 
+  <div class="content-under-navbar px-[15px] pt-10 md:!px-64 md:pt-40">
+    <navbar class="fixed-navbar px-[15px] md:px-64" /> 
     <router-view class=""/> 
 
     <br><br><br><br><br>
