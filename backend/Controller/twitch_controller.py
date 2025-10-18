8a350c84f98ce60af36fc01257b286d577f406d1
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-18 17:33:52 +0200

    store user_id in clip table

--- backend/Controller/twitch_controller.py
+++ backend/Controller/twitch_controller.py
@@ -41,7 +41,7 @@ class TwitchController():
             for clip in fetched_clips:
                 
                 clip_service = ClipServices(db)
-                clip = clip_service.download_and_store_clip(clip)
+                clip = clip_service.download_and_store_clip(clip,request.user_id)
                 
 
             return jsonify({
