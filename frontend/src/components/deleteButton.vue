168da4603a7d55e9cf81a9447c3c59de12b62784
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-28 14:38:19 +0100

    add delete button for previewclips

---
+++ frontend/src/components/deleteButton.vue
@@ -0,0 +1,26 @@
+
+<script setup>
+import { XMarkIcon } from '@heroicons/vue/24/solid';
+
+
+const props = defineProps({
+
+    width:{
+        type:String,
+        required:true
+    },
+    height:{
+        type:String,
+        required:true
+    }
+
+})
+</script>
+
+
+<template>
+
+    <button class="w-[30px] h-[30px] bg-red-700 rounded-sm transform transition-transform duration-300 hover:scale-105 ">
+       <XMarkIcon class="text-white"></XMarkIcon>
+    </button>
+</template>
