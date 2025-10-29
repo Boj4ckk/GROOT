f390dda2405d3ee3ea4e1d08e246358a57d48531
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-29 20:07:48 +0100

    update responsive for the app + remove scroll

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import navbar from './components/navbar.vue';
 </script>
 
 <template>
-  <div class="content-under-navbar px-[15px] pt-10 md:!px-20 lg:!px-30 xl:!px-80 md:pt-40">
+  <div class="content-under-navbar px-[15px] pt-10 md:!px-20 lg:!px-10 xl:!px-40 md:pt-40">
     <navbar class="fixed-navbar px-[15px] md:px-20 lg:!px-30 xl:!px-80 " /> 
     <router-view class=""/> 
 
@@ -13,3 +13,15 @@ import navbar from './components/navbar.vue';
  
 
 </template>
+
+<style>
+html,
+body {
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  height: 100%;
+}
+</style>
