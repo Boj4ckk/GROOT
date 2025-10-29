a2a352b9508261a978598c36c2924c5da7a60834
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-29 13:34:26 +0100

    fix responsiv for navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,8 @@ import navbar from './components/navbar.vue';
 </script>
 
 <template>
-  <div class="content-under-navbar px-[15px] pt-10 md:!px-64 md:pt-40">
-    <navbar class="fixed-navbar px-[15px] md:px-64" /> 
+  <div class="content-under-navbar px-[15px] pt-10 md:!px-20 lg:!px-30 xl:!px-80 md:pt-40">
+    <navbar class="fixed-navbar px-[15px] md:px-20 lg:!px-30 xl:!px-80 " /> 
     <router-view class=""/> 
 
     <br><br><br><br><br>
