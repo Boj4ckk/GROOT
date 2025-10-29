555770f60ed4ef9ce7991b63aa2822d432402994
yazkilito@gmail.com <yazkilito@gmail.com>
2025-10-29 20:08:18 +0100

    update deletebutton for responsive

--- frontend/src/components/deleteButton.vue
+++ frontend/src/components/deleteButton.vue
@@ -1,26 +1,11 @@
 
 <script setup>
 import { XMarkIcon } from '@heroicons/vue/24/solid';
-
-
-const props = defineProps({
-
-    width:{
-        type:String,
-        required:true
-    },
-    height:{
-        type:String,
-        required:true
-    }
-
-})
 </script>
 
-
 <template>
-
-    <button class="w-[30px] h-[30px] bg-red-700 rounded-sm transform transition-transform duration-300 hover:scale-105 ">
-       <XMarkIcon class="text-white"></XMarkIcon>
+    <button 
+        class="w-10 h-10 md:w-12 md:h-12 bg-red-700 rounded-md transform transition-transform duration-300 hover:scale-105 flex items-center justify-center aspect-square flex-shrink-0">
+       <XMarkIcon class="text-white w-3/4 h-3/4" />
     </button>
 </template>
