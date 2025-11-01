27f0143bac0724c3a430bda6ef4422834f240c28
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-01 19:03:51 +0100

    move custombutton into dedicated folder : Buttons

---
+++ frontend/src/components/Buttons/deleteButton.vue
@@ -0,0 +1,11 @@
+
+<script setup>
+import { XMarkIcon } from '@heroicons/vue/24/solid';
+</script>
+
+<template>
+    <button 
+        class="w-10 h-10 md:w-12 md:h-12 bg-red-700 rounded-md transform transition-transform duration-300 hover:scale-105 flex items-center justify-center aspect-square flex-shrink-0">
+       <XMarkIcon class="text-white w-3/4 h-3/4" />
+    </button>
+</template>
